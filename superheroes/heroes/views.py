f0e5ce68ac2a7c6fde54a3ffeb8f0022249d825d
wrightzachary <taylorzw96@gmail.com>
2021-06-29 10:18:29 -0500

    Changed object to objects in views.py and added bootstrap link to index.html

--- superheroes/heroes/views.py
+++ superheroes/heroes/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Heroes
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'heroes/index.html')
+    all_heroes = Heroes.objects.all()
+    context = {
+        'all_heroes': all_heroes
+    }
+    return render(request, 'heroes/index.html', context)
