507ef189933b5432a7c6aa6ad4deeaf858d7456d
wrightzachary <taylorzw96@gmail.com>
2021-06-29 00:51:53 -0500

    set up SQL Database

--- superheroes/superheroes/settings.py
+++ superheroes/superheroes/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'superheroes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'superheroes_db',
+        'USER': 'root',
+        'PASSWORD': 'password123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
     }
 }
 
