811369cae4684f1b337cdc63448eeedb24bd25a0
wrightzachary <taylorzw96@gmail.com>
2021-06-29 01:07:18 -0500

    created Heroes class and admin site

--- superheroes/heroes/models.py
+++ superheroes/heroes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Heroes(models.Model):
+    name = models.CharField(max_length=50)
+    alter_ego = models.CharField(max_length=50)
+    primary_superhero_ability = models.CharField(max_length=50)
+    secondary_superhero_ability = models.CharField(max_length=50)
+    catchphrase = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
